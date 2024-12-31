299810720b876b1b92870c68998189d7f834e620
Tristan Miche <tmiche@ippon.fr>
2024-12-31 18:08:57 +0100

    feat(chat):  use claude3 for chat module

--- backend/chat_playground/routes.py
+++ backend/chat_playground/routes.py
@@ -1,7 +1,13 @@
+import sys
+sys.path.append("../fm")
+
+
 from fastapi import APIRouter, HTTPException
 from botocore.exceptions import ClientError
 from . import models
 from . import services
+
+from fm import claude3
 
 router = APIRouter()
 
@@ -10,6 +16,19 @@ router = APIRouter()
 def invoke(body: models.ChatRequest):
     try:
         completion = services.invoke(body.prompt)
+        return models.ChatResponse(
+            completion=completion
+        )
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "AccessDeniedException":
+            raise HTTPException(status_code=403)
+        else:
+            raise HTTPException(status_code=500)
+
+@router.post("/foundation-models/model/chat/anthropic.claude-v3/invoke")
+def invoke(body: models.ChatRequest):
+    try:
+        completion = claude3.invoke(body.prompt)
         return models.ChatResponse(
             completion=completion
         )
