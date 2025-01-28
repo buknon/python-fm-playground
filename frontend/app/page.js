2aa9d8ceb0a94e1fe69aa2dfe1cccf6fa6426e3d
Tristan Miche <tmiche@ippon.fr>
2025-01-28 19:10:48 +0100

    chore(front): change title

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
       <div className="container px-6 py-8 mx-auto">
-          <h3 className="text-3xl font-medium text-gray-700">Welcome to the Python FM Playground!</h3>
+          <h3 className="text-3xl font-medium text-gray-700">FM Playground</h3>
           <div className="mt-8">
               The Python Foundation Model (FM) Playground is a sample application showcasing how to leverage Amazon
               Bedrock using Python. As any sample application, it is not production-ready. It is provided
